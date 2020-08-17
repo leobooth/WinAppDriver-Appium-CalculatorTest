46008ecd9e89036639ec0da9692819519cb15ae3
lbooth@dealersocket.com <lbooth@dealersocket.com>
2020-08-16 19:22:00 -0500

    updated gitignore

--- src/main/java/org/automate/App.java
+++ src/main/java/org/automate/App.java
@@ -1,13 +1,9 @@
 package org.automate;
 
-/**
- * Hello world!
- *
+/** Hello world!
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
 }
--- src/test/java/org/automate/CalculatorTest.java
+++ src/test/java/org/automate/CalculatorTest.java
@@ -25,104 +25,94 @@ import io.appium.java_client.windows.WindowsDriver;
 
 public class CalculatorTest {
 
-    private static WindowsDriver CalculatorSession = null;
-    private static WebElement CalculatorResult = null;
+  private static WindowsDriver CalculatorSession = null;
+  private static WebElement CalculatorResult = null;
 
-    @BeforeClass
-    public static void setup() {
-        try {
-            DesiredCapabilities capabilities = new DesiredCapabilities();
-            capabilities.setCapability("app", "Microsoft.WindowsCalculator_8wekyb3d8bbwe!App");
-            CalculatorSession = new WindowsDriver(new URL("http://127.0.0.1:4723"), capabilities);
-            CalculatorSession.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+  @BeforeClass
+  public static void setup() {
+    try {
+      DesiredCapabilities capabilities = new DesiredCapabilities();
+      capabilities.setCapability("app", "Microsoft.WindowsCalculator_8wekyb3d8bbwe!App");
+      CalculatorSession = new WindowsDriver(new URL("http://127.0.0.1:4723"), capabilities);
+      CalculatorSession.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
 
-            CalculatorResult = CalculatorSession.findElementByAccessibilityId("CalculatorResults");
-            Assert.assertNotNull(CalculatorResult);
+      CalculatorResult = CalculatorSession.findElementByAccessibilityId("CalculatorResults");
+      Assert.assertNotNull(CalculatorResult);
 
-        }catch(Exception e){
-            e.printStackTrace();
-        } finally {
-        }
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+  }
 
-    @Before
-    public void Clear()
-    {
-        CalculatorSession.findElementByName("Clear").click();
-        Assert.assertEquals("0", _GetCalculatorResultText());
+  @Before
+  public void Clear() {
+    CalculatorSession.findElementByName("Clear").click();
+    Assert.assertEquals("0", FormatCalculatorResultsText());
+  }
+
+  @AfterClass
+  public static void TearDown() {
+    CalculatorResult = null;
+    if (CalculatorSession != null) {
+      CalculatorSession.quit();
     }
+    CalculatorSession = null;
+  }
 
-    @AfterClass
-    public static void TearDown()
-    {
-        CalculatorResult = null;
-        if (CalculatorSession != null) {
-            CalculatorSession.quit();
-        }
-        CalculatorSession = null;
-    }
+  @Test
+  public void Addition() {
+    CalculatorSession.findElementByName("One").click();
+    CalculatorSession.findElementByName("Plus").click();
+    CalculatorSession.findElementByName("Seven").click();
+    CalculatorSession.findElementByName("Equals").click();
+    Assert.assertEquals("8", FormatCalculatorResultsText());
+  }
 
-    @Test
-    public void Addition()
-    {
-        CalculatorSession.findElementByName("One").click();
-        CalculatorSession.findElementByName("Plus").click();
-        CalculatorSession.findElementByName("Seven").click();
-        CalculatorSession.findElementByName("Equals").click();
-        Assert.assertEquals("8", _GetCalculatorResultText());
-    }
+  @Test
+  public void Combination() {
+    CalculatorSession.findElementByName("Seven").click();
+    CalculatorSession.findElementByName("Multiply by").click();
+    CalculatorSession.findElementByName("Nine").click();
+    CalculatorSession.findElementByName("Plus").click();
+    CalculatorSession.findElementByName("One").click();
+    CalculatorSession.findElementByName("Equals").click();
+    CalculatorSession.findElementByName("Divide by").click();
+    CalculatorSession.findElementByName("Eight").click();
+    CalculatorSession.findElementByName("Equals").click();
+    Assert.assertEquals("8", FormatCalculatorResultsText());
+  }
 
-    @Test
-    public void Combination()
-    {
-        CalculatorSession.findElementByName("Seven").click();
-        CalculatorSession.findElementByName("Multiply by").click();
-        CalculatorSession.findElementByName("Nine").click();
-        CalculatorSession.findElementByName("Plus").click();
-        CalculatorSession.findElementByName("One").click();
-        CalculatorSession.findElementByName("Equals").click();
-        CalculatorSession.findElementByName("Divide by").click();
-        CalculatorSession.findElementByName("Eight").click();
-        CalculatorSession.findElementByName("Equals").click();
-        Assert.assertEquals("8", _GetCalculatorResultText());
-    }
+  @Test
+  public void Division() {
+    CalculatorSession.findElementByName("Eight").click();
+    CalculatorSession.findElementByName("Eight").click();
+    CalculatorSession.findElementByName("Divide by").click();
+    CalculatorSession.findElementByName("One").click();
+    CalculatorSession.findElementByName("One").click();
+    CalculatorSession.findElementByName("Equals").click();
+    Assert.assertEquals("8", FormatCalculatorResultsText());
+  }
 
-    @Test
-    public void Division()
-    {
-        CalculatorSession.findElementByName("Eight").click();
-        CalculatorSession.findElementByName("Eight").click();
-        CalculatorSession.findElementByName("Divide by").click();
-        CalculatorSession.findElementByName("One").click();
-        CalculatorSession.findElementByName("One").click();
-        CalculatorSession.findElementByName("Equals").click();
-        Assert.assertEquals("8", _GetCalculatorResultText());
-    }
+  @Test
+  public void Multiplication() {
+    CalculatorSession.findElementByName("Nine").click();
+    CalculatorSession.findElementByName("Multiply by").click();
+    CalculatorSession.findElementByName("Nine").click();
+    CalculatorSession.findElementByName("Equals").click();
+    Assert.assertEquals("81", FormatCalculatorResultsText());
+  }
 
-    @Test
-    public void Multiplication()
-    {
-        CalculatorSession.findElementByName("Nine").click();
-        CalculatorSession.findElementByName("Multiply by").click();
-        CalculatorSession.findElementByName("Nine").click();
-        CalculatorSession.findElementByName("Equals").click();
-        Assert.assertEquals("81", _GetCalculatorResultText());
-    }
+  @Test
+  public void Subtraction() {
+    CalculatorSession.findElementByName("Nine").click();
+    CalculatorSession.findElementByName("Minus").click();
+    CalculatorSession.findElementByName("One").click();
+    CalculatorSession.findElementByName("Equals").click();
+    Assert.assertEquals("8", FormatCalculatorResultsText());
+  }
 
-    @Test
-    public void Subtraction()
-    {
-        CalculatorSession.findElementByName("Nine").click();
-        CalculatorSession.findElementByName("Minus").click();
-        CalculatorSession.findElementByName("One").click();
-        CalculatorSession.findElementByName("Equals").click();
-        Assert.assertEquals("8", _GetCalculatorResultText());
-    }
-
-    protected String _GetCalculatorResultText()
-    {
-        // trim extra text and whitespace off of the display value
-        return CalculatorResult.getText().replace("Display is", "").trim();
-    }
-
+  protected String FormatCalculatorResultsText() {
+    // trim extra text and whitespace off of the display value
+    return CalculatorResult.getText().replace("Display is", "").trim();
+  }
 }
